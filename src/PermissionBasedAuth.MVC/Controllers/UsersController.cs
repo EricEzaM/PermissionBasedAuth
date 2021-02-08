3ca6264414c06c2f1e7c52cb0c0f7646691c0c63
Eric M <itsjusteza@gmail.com>
2021-02-08 22:29:24 +1000

    Added pages for user and user role management.

---
+++ src/PermissionBasedAuth.MVC/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PermissionBasedAuth.MVC.Areas.Identity.Data;
+using PermissionBasedAuth.MVC.Permission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermissionBasedAuth.MVC.Controllers
+{
+	[HasPermission(Permissions.Test.SuperAdminOnly)]
+	public class UsersController : Controller
+	{
+		private readonly UserManager<PermissionAuthUser> _userManager;
+
+		public UsersController(UserManager<PermissionAuthUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public IActionResult Index()
+		{
+			var allUsers = _userManager
+				.Users
+				.ToList()
+				.OrderBy(u => u.UserName); // Consistent ordering on refresh
+
+			return View(allUsers);
+		}
+	}
+}
---
+++ src/PermissionBasedAuth.MVC/Models/UserRolesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PermissionBasedAuth.MVC.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermissionBasedAuth.MVC.Models
+{
+	public class UserRolesViewModel
+	{
+		public PermissionAuthUser User { get; set; }
+		public List<UserRoleViewModel> UserRoles { get; set; }
+	}
+
+	public class UserRoleViewModel
+	{
+		public string RoleName { get; set; }
+		public bool Active { get; set; }
+	}
+}
