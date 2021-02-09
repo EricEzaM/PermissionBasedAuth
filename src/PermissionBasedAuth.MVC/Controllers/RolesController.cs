aae25ce78efb08dd468f543d4cea8efcb48a99e9
Eric M <itsjusteza@gmail.com>
2021-02-09 13:54:26 +1000

    Added roles controller with views for viewing all roles and editing role permissions.

---
+++ src/PermissionBasedAuth.MVC/Controllers/RolesController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PermissionBasedAuth.MVC.Areas.Identity.Data;
+using PermissionBasedAuth.MVC.Models;
+using PermissionBasedAuth.MVC.Permission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PermissionBasedAuth.MVC.Controllers
+{
+	public class RolesController : Controller
+	{
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public RolesController(RoleManager<IdentityRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return View(_roleManager.Roles.ToList());
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Update(string roleId)
+		{
+			var role = await _roleManager.FindByIdAsync(roleId);
+			var roleClaimValues = (await _roleManager.GetClaimsAsync(role))
+				.Where(c => c.Type == "Permission")
+				.Select(c => c.Value);
+
+			RolePermissionsViewModel vm = new()
+			{
+				RoleId = role.Id,
+				RoleName = role.Name,
+				Permissions = PermissionDisplay.GetPermissionDisplays(typeof(Permissions))
+					.Select(pd => new RolePermissionViewModel()
+					{
+						Name = pd.Name,
+						GroupName = pd.GroupName,
+						Description = pd.Description,
+						Value = pd.Value,
+						Active = roleClaimValues.Contains(pd.Value)
+					}).ToList()
+			};
+
+			return View(vm);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Update(string roleId, RolePermissionsViewModel model)
+		{
+			var role = await _roleManager.FindByIdAsync(roleId);
+
+			// Get Current Permission Claims
+			var currentPermissionClaims = (await _roleManager.GetClaimsAsync(role))
+				.Where(c => c.Type == CustomClaimTypes.Permission)
+				.Select(c => c.Value);
+			var newPermissionClaims = model.Permissions
+				.Where(p => p.Active)
+				.Select(p => p.Value);
+
+			var claimValuesToAdd = newPermissionClaims.Except(currentPermissionClaims);
+			var claimValuesToRemove = currentPermissionClaims.Except(newPermissionClaims);
+
+			foreach (var claimValue in claimValuesToRemove)
+			{
+				await _roleManager.RemoveClaimAsync(role, new Claim(CustomClaimTypes.Permission, claimValue));
+			}
+
+			foreach (var claimValue in claimValuesToAdd)
+			{
+				await _roleManager.AddClaimAsync(role, new Claim(CustomClaimTypes.Permission, claimValue));
+			}
+
+			TempData["SuccessMessage"] = "Permissions updated.";
+			return RedirectToAction(nameof(Update), new { roleId });
+		}
+
+		[HttpPost]
+		public IActionResult Delete(string roleId)
+		{
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
---
+++ src/PermissionBasedAuth.MVC/Models/RolePermissionsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PermissionBasedAuth.MVC.Permission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermissionBasedAuth.MVC.Models
+{
+	public class RolePermissionsViewModel
+	{
+		public string RoleId { get; set; }
+		public string RoleName { get; set; }
+		public List<RolePermissionViewModel> Permissions { get; set; }
+	}
+
+	public class RolePermissionViewModel
+	{
+		public string GroupName { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Value { get; set; }
+		public bool Active { get; set; }
+	}
+}
