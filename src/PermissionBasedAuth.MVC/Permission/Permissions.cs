b88dd03d11f19cd1144da2dc0a91e9183f49df0a
Eric M <itsjusteza@gmail.com>
2021-02-09 13:53:46 +1000

    Added additional permissions for creating and editing roles.

--- src/PermissionBasedAuth.MVC/Permission/Permissions.cs
+++ src/PermissionBasedAuth.MVC/Permission/Permissions.cs
@@ -18,5 +18,19 @@ namespace PermissionBasedAuth.MVC.Permission
 			[Display(GroupName = "Test", Name = "Test Two", Description = "Second Test Permission")]
 			public const string SuperAdminOnly = "Permissions.Test.SuperAdminOnly";
 		}
+
+		public static class Roles
+		{
+			[Display(GroupName = "Roles", Name = "Create", Description = "Create new roles.")]
+			public const string Create = "Permissions.Roles.Create";
+			
+			public static class Edit
+			{
+				[Display(GroupName = "Roles", Name = "Edit Metadata", Description = "Edit role metdata.")]
+				public const string Metadata = "Permissions.Roles.Edit.Medatata";
+				[Display(GroupName = "Roles", Name = "Edit Permissions", Description = "Edit which permissions are assigned to each role.")]
+				public const string Permissions = "Permissions.Roles.Edit.Permissions";
+			}
+		}
 	}
 }
